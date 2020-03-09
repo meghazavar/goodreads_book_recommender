1226222fe6331c617a6f4efb9742eb5a5d48b0e5
Megha Zavar <mybiyani@Meghas-MacBook-Air.local>
2020-03-09 15:43:13 -0600

    data cleaning changes

--- code/recommenders.py
+++ code/recommenders.py
@@ -11,18 +11,27 @@ class  helper
 
     def find_titles(self,books, title):
         titles = books.loc[books['title'].str.lower().str.contains(title),'title'].tolist()
-        return titles    
+        return titles
+    def search_by_title(title):
+        l= ds.loc[ds['title'].str.lower().str.contains(title),['title','book_id']].tolist()
+        return l
+
+    def book_title(book_id):
+        return ds.loc[ds['book_id'] == book_id,'title'].values[0]
 
 class collab_recommender:
     """docstring for ."""
-
 
     def __init__(self):
         print("in constructor collab_recommender")
 
 
 
+class collab_recommender:
+    """docstring for ."""
 
+    def __init__(self):
+        print("in constructor collab_recommender")
 
 def main():
   print("Hello World!")
